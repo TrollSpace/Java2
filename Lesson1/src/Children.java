5263630bdb782555f66d2002b39a35cda6757968
TrollSpace <miger2000@mail.ru>
2018-08-16 00:34:14 +0300

    Create polymorphism.

--- Lesson1/src/Children.java
+++ Lesson1/src/Children.java
@@ -2,8 +2,19 @@
  * Created by Misha on 16.08.2018.
  */
 public class Children extends People {
+    int size;
+
     public Children(String name, int age) {
         super(name, age);
+        this.size = 5;
+    }
+
+    public boolean isEmpty() {
+        if (size > 0) {
+            size--;
+            return false;
+        }
+        return true;
     }
 
 }
